d40ed689c2af9c183774105694f3c296272ae68a
Herco Bezuidenhout <hercobez@icloud.com>
2025-05-06 19:55:53 +0200

    feat: Play around with middleware

--- api/routes/web.php
+++ api/routes/web.php
@@ -1,9 +1,14 @@
 <?php
 
+use App\Http\Middleware\HelloWorldMiddleware;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HelloWorldController;
 
 Route::controller(HelloWorldController::class)->group(function () {
     Route::get('/hello-world', 'index');
-    Route::get('/hello-world/{name}', 'getName');
+    Route::get('/hello-world/{name}', 'getName')->middleware(HelloWorldMiddleware::class);
+});
+
+Route::fallback(function () {
+    return "Welcome to the Teamtjie API";
 });
